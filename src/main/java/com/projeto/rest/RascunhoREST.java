3246c2f6c55e77797bf118805a6877d475200168
mustella <jonas.soares@hepta.com.br>
2021-09-10 15:09:44 -0300

    Servlet teste funcional

--- src/main/java/com/projeto/rest/RascunhoREST.java
+++ src/main/java/com/projeto/rest/RascunhoREST.java
@@ -1,11 +1,12 @@
 package com.projeto.rest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,13 +18,10 @@ import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class RascunhoREST {
-    
-    @Context
-    HttpHeaders headers;
 
     @GET
     public Response testaREST(@QueryParam String param) {
-      return Response.ok().build();
+        return Response.ok("Teste funcional!", MediaType.TEXT_PLAIN).build();
     }
 
 }
