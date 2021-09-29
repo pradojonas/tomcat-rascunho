86692edce80ff2b7b282721249528f83b1ff7c71
mustella <pradojonas64@gmail.com>
2021-09-29 11:47:13 -0300

    Bugfix

--- client-side/src/services/RascunhoService.js
+++ client-side/src/services/RascunhoService.js
@@ -3,7 +3,7 @@ const pathBase = "/rascunho-projeto";
 
 export default () => {
   async function getRascunho(queryParam) {
-    return await api.get(`${pathBase}/rascunho`);
+    return await api().get(`${pathBase}/rascunho`);
   }
 
   return { getRascunho };
